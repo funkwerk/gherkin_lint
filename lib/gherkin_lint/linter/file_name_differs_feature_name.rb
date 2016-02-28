bb4a8be436eb51215cd13454cc32e57df621f0ab
stefan <think@hotmail.de>
2016-02-28 15:58:02 +0100

    fixed usage of casecmp

--- lib/gherkin_lint/linter/file_name_differs_feature_name.rb
+++ lib/gherkin_lint/linter/file_name_differs_feature_name.rb
@@ -7,7 +7,7 @@ module GherkinLint
       features do |file, feature|
         next unless feature.include? 'name'
         expected_feature_name = title_case file
-        next unless feature['name'].casecmp expected_feature_name
+        next if feature['name'].casecmp(expected_feature_name) == 0
         references = [reference(file, feature)]
         add_issue(references, "Feature name should be '#{expected_feature_name}'")
       end
