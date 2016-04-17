04a61225c7e6525acb513919a7f397228b1e3c07
stefan <think@hotmail.de>
2016-04-17 20:40:41 +0200

    using tagger as instance variable and initialize it lazy

--- lib/gherkin_lint/linter/be_declarative.rb
+++ lib/gherkin_lint/linter/be_declarative.rb
@@ -4,6 +4,10 @@ require 'engtagger'
 module GherkinLint
   # service class to lint for avoiding periods
   class BeDeclarative < Linter
+    def initialize
+      super
+    end
+
     def lint
       filled_scenarios do |file, feature, scenario|
         scenario['steps'].each do |step|
@@ -14,14 +18,13 @@ module GherkinLint
     end
 
     def verb?(step)
-      tagger = EngTagger.new
       tagged = tagger.add_tags step['name']
-      step_verbs = verbs(tagger, tagged)
+      step_verbs = verbs tagged
 
       !step_verbs.empty?
     end
 
-    def verbs(tagger, tagged_text)
+    def verbs(tagged_text)
       verbs = [
         :get_infinitive_verbs,
         :get_past_tense_verbs,
@@ -31,6 +34,12 @@ module GherkinLint
         :get_base_present_verbs
       ]
       verbs.map { |verb| tagger.send(verb, tagged_text).keys }.flatten
+    end
+
+    def tagger
+      @tagger = EngTagger.new unless instance_variable_defined? :@tagger
+
+      @tagger
     end
   end
 end
