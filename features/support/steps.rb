70f625a3b07e4ae2302c9c5c4529d1f1ee1a5100
Eric Kessler <morrow748@gmail.com>
2017-11-12 21:14:10 -0500

    Don't break on non-Windows
    Forgot to make the Windows fixes conditional.

--- features/support/steps.rb
+++ features/support/steps.rb
@@ -1,6 +1,6 @@
 Then(/^it should fail with exactly:$/) do |expected|
   # Standardize line endings
-  expected = expected.gsub("\n", "\r\n")
+  expected = expected.gsub("\n", "\r\n") if RUBY_PLATFORM =~ /mingw32/
 
   expect(last_command_stopped).not_to be_successfully_executed
   expect(last_command_stopped).to have_output an_output_string_being_eq(expected)
@@ -8,7 +8,7 @@ end
 
 Then(/^it should pass with exactly:$/) do |expected|
   # Standardize line endings
-  expected = expected.gsub("\n", "\r\n")
+  expected = expected.gsub("\n", "\r\n") if RUBY_PLATFORM =~ /mingw32/
 
   expect(last_command_stopped).to be_successfully_executed
   expect(last_command_stopped).to have_output an_output_string_being_eq(expected)
